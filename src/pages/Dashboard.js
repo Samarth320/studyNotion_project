ec10378906e7f09553da6dca988b3a3ebe368d56
Samarth320 <samarthbagul49@gmail.com>
2024-06-28 20:17:08 +0530

    first commit

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+ import React from 'react'
+ 
+ const Dashboard = () => {
+   return (
+     <div className='w-screen min-h-screen flex flex-col justify-center items-center text-white text-3xl'>
+        Welcome to StudyNotion
+     </div>
+   )
+ }
+ 
+ export default Dashboard
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='w-screen min-h-screen flex flex-col justify-center items-center text-white text-3xl'>
+      Home
+    </div>
+  )
+}
+
+export default Home
